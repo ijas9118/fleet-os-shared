37b98140cbcd9703831f00cd2d07a966f624f73a
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2025-12-06 20:46:25 +0530

    add shipment status

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { UserRole } from "./user-role.enum";
 export { TenantStatus } from "./tenant-status.enum";
+export { ShipmentStatus } from "./shipment-status.enum";
 export { STATUS_CODES } from "./status-codes.constant";
